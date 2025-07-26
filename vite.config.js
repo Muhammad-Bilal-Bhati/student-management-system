2160d45326b29e97764f06e29e56b3b0c4375863
= <bilalbhattilive@gmail.com>
2025-07-26 21:04:09 +0500

    student management system

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,15 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     define: {
-      // Make env variables available as process.env.VARIABLE_NAME
-      'process.env': Object.keys(env).reduce((acc, key) => {
-        acc[key] = JSON.stringify(env[key])
-        return acc
-      }, {})
+      // Make specific env variables available as process.env.VARIABLE_NAME
+      'process.env.FIREBASE_API_KEY': JSON.stringify(env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(env.FIREBASE_APP_ID),
+      'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(env.FIREBASE_MEASUREMENT_ID),
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV || mode)
     }
   }
 })
